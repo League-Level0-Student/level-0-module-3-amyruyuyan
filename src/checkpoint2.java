4041222662f5649a589d1ded0f6a8ea2a98ff328
amyruyuyan <league@WTS14-2.attlocal.net>
2019-03-24 14:28:21 -0700

    checkpoints

---
+++ src/checkpoint2.java
@@ -0,0 +1,17 @@
+
+public class checkpoint2 {
+public static void main(String[] args) {
+for (int i = 1; i < 9; i++)	{
+if (i == 4) { 	
+System.out.println(i);	
+}
+else if (i < 8) {
+System.out.print(i + " potato, ");
+}
+else if (i ==8) {
+System.out.println(" more");	
+}
+}
+}
+}
+
